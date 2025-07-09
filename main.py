441dab888179bb107082b2a0525c6be96478ccbd
xxraytz <eg-su@mail.ru>
2025-07-09 16:50:08 +0000

    Rename files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import argparse
 
-from scaled.runner import run as run_scaled
+from sql_toolset.runner import run as run_scaled
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     parser.add_argument("--max-iter", type=int, default=10)
     parser.add_argument("--method", type=str, default="scaled")
     args = parser.parse_args()
-    if args.method == "scaled":
+    if args.method == "toolkit":
         run_scaled(args.query, db_uri=args.db, max_iter=args.max_iter)
     else:
-        from full_context_based.core import main as run_buildin_context
+        from builtin_context.core import main as run_buildin_context
 
         assert run_buildin_context is not None
         run_buildin_context(args.query)
